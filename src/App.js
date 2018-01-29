81ab0a751ebb54263ecd0d7bb07884879450e84d
dansbands <dansbands@hotmail.com>
2018-01-29 18:31:29 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput'
+import { ConnectedUsers } from './components/Users'
 
 export class App extends Component {
   render() {
+    console.log('App', this.props);
     return (
       <div className="App">
         <UserInput store={this.props.store}/>
-        {/* is there something we could connect to here? */}
+        <ConnectedUsers />
       </div>
     );
   }
 }
 
+// const mapStateToProps = state => {
+//   return { users: state.users }
+// }
+//
+// export default connect(mapStateToProps)(App);
 export default App;
